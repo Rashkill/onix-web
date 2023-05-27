f52378304e1af4dd84ded7b0a0c9a11ef1e27888
Juan Becchio <juan.becchio@4rsoluciones.com>
2023-05-26 23:45:27 -0300

    Iniciando con la home

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,23 @@
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="home-page">
+      <header>
+        <h1>
+          Bienvenidx a <span color="pink">Onix</span>
+        </h1>
+        <h2>Tienda de Arte & Ilustraciones Propias</h2>
+        <p>
+          Nuestro objetivo es ofrecer productos con diseños e ilustraciones{" "}
+          <b>100% originales</b> y dar a conocer nuestro arte. NO ofrecemos
+          servicios de imprenta ni venta mayorista
+        </p>
+        <div>
+          <button color="primary">Contactanos</button>
+          <button color="secondary">Pedí tu personalizado</button>
+        </div>
+      </header>
+    </div>
+  );
 };
 
 export default Home;
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -3,22 +3,24 @@ import routes from "./routes";
 import Wrapper from "./Wrapper";
 import "./router.scss";
 
+const routesScreens = routes.map(
+  ({ path, element, children, noHeader, name, color }) => ({
+    path,
+    element: (
+      <Wrapper showHeader={!noHeader} headerInfo={{ title: name, color }} />
+    ),
+    children: [
+      { path, element },
+      ...(children || [])?.map((c) => ({
+        path: `${path}${c.path}`,
+        element: c.element,
+      })),
+    ],
+  })
+);
+console.log(routesScreens);
 const Router = () => {
-  return useRoutes(
-    routes.map(({ path, element, children, noHeader, name, color }) => ({
-      path,
-      element: (
-        <Wrapper showHeader={!noHeader} headerInfo={{ title: name, color }} />
-      ),
-      children: [
-        { path, element },
-        ...(children || [])?.map((c) => ({
-          path: `${path}${c.path}`,
-          element: c.element,
-        })),
-      ],
-    }))
-  );
+  return useRoutes(routesScreens);
 };
 
 export default Router;
